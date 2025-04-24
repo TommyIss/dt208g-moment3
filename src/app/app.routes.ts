1250494ea3501454f0c10ace1a00465fc384592e
Tommy Issa <tommyissa@hotmail.com>
2025-04-24 12:14:36 +0200

    Har skapat komponenter för startsida, undersidor, samt för semantiska element "header" och "footer"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ConvertComponent } from './pages/convert/convert.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'about', component: AboutComponent},
+    {path: 'convert', component: ConvertComponent},
+    {path: '404', component: NotfoundComponent},
+    {path: '**', component: NotfoundComponent}
+];
