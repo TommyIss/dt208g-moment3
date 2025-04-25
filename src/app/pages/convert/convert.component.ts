37cb6e7946d699f98707b9e17ba9a15ba56d946e
Tommy Issa <tommyissa@hotmail.com>
2025-04-25 19:06:44 +0200

    Har skapat innehåll(inmatningsfält), stillsättning samt funktionalitet för konverterings sida/komponent

--- src/app/pages/convert/convert.component.ts
+++ src/app/pages/convert/convert.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.scss'
 })
 export class ConvertComponent {
-
+  // Inputsvärde
+  length: number = 0;
+  temp: number = 0;
+  result: number = 0;
+  unit: string = '';
+  // Metoder
+  feetToMeter() {
+    this.result = this.length*0.3048;
+    this.unit = 'meter';
+  }
+  meterToFeet(): void {
+    this.result = this.length*3.28084;
+    this.unit = 'fot';
+  }
+  fahrenheitToCelsius(): void {
+    this.result = (this.temp - 32) * 5/9;
+    this.unit = '°C';
+  }
+  celeciusToFahrenhei(): void {
+    this.result = (this.temp * (9/5)) + 32;
+    this.unit = '°F'; 
+  }
 }
