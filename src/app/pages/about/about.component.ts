00da17ad1d21fa203bbe6ca180bb3093f5c1f11b
Tommy Issa <tommyissa@hotmail.com>
2025-05-01 11:10:53 +0200

    Har skapt innehåll för about-html, har även fixat navigeringsmeny i mindre skärm så att öppnar och stänger med knappar

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  imagePath: string = 'images/research.jpg';
+  imagePath1: string = 'images/research.jpg?as=avif';
+  imagePath2: string = 'images/research.jpg?as=webp';
 }
--- src/app/semantic-elements/header/header.component.ts
+++ src/app/semantic-elements/header/header.component.ts
@@ -8,68 +8,66 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  // openBtn = document.getElementById('open-btn') as HTMLButtonElement;
-  // closeBtn = document.getElementById('close-btn') as HTMLButtonElement;
-  // navMenu = document.getElementById('nav-menu') as HTMLDivElement;
-  // @ViewChild('open-btn') openBtn!: ElementRef<HTMLButtonElement>;
-  // @ViewChild('close-btn') closeBtn!: ElementRef<HTMLButtonElement>;
-  // @ViewChild('nav-menu') navMenu!: ElementRef<HTMLDivElement>;
-
-  // ngAfterViewInit(): void {
-  //   if (!this.openBtn) {
-  //     console.error(`${this.openBtn} saknas!`);
-      
-  //   } else if(!this.closeBtn) {
-  //     console.error(`${this.closeBtn} saknas!`);
-  //   } {
-  //     this.openBtn.nativeElement.addEventListener('click', this.menuToggle.bind(this));
-  //     this.closeBtn.nativeElement.addEventListener('click', this.menuToggle.bind(this));
-  //   }
-  // }
-
-  // menuToggle(): void {
-  //   if (!this.navMenu) {
-  //     console.error('navMenu saknas');
-      
-  //   }
-    
-  //   let style = window.getComputedStyle(this.navMenu.nativeElement);
-
-  //   if(style.display === 'none') {
-  //   this.navMenu.nativeElement.style.display = 'block';
-  // } else {
-  //   this.navMenu.nativeElement.style.display = 'none';
-  // }
-  // }
+  // Meny default-display
+  menuDisplay: string = 'none';
   
+  // Skärm är 800px eller mindre
+  mediaQuery = window.matchMedia('(max-width: 800px)');
+  
+  // Funktion för menyen vid sidans start/omladdning
+  init(): void {
+    if(this.mediaQuery.matches) {
+      this.menuDisplay = 'none';
+    } else {
+      this.menuDisplay = 'block';
+    }
+  }
+
+  // Uppdatera menyn när skärmstorlek ändras
+  updateMenuDisplay(isSmallScreen: boolean): void {
+    let navMenuEl = document.getElementById('nav-menu') as HTMLDivElement;
+    if(isSmallScreen) {
+      this.menuDisplay = 'none';
+      navMenuEl.style.display = 'none';
+    } else {
+      this.menuDisplay = 'block';
+      navMenuEl.style.display = 'block';
+    }
+  }
+
+  // Anrop för funktioner när sidans startas/omladdas
+  ngOnInit(): void {
+    this.init();
+
+    this.updateMenuDisplay(this.mediaQuery.matches);
+    this.mediaQuery.addEventListener('change', (event: MediaQueryListEvent) => { 
+      this.updateMenuDisplay(event.matches);
+    });
+
+    document.addEventListener('click', this.closeMenuOnClickOutside.bind(this));
+  }
+
+  // Öppna/ stänga menu
+  menuToggle(): void {
+    let navMenuEl = document.getElementById('nav-menu') as HTMLDivElement;
+    if(this.menuDisplay === 'none') {
+      this.menuDisplay = 'block';
+      navMenuEl.style.display = 'block';
+    } else {
+      this.menuDisplay = 'none';
+      navMenuEl.style.display = 'none';
+    }
+  }
+
+  // Stäng menyn när man klickar utanför
+  closeMenuOnClickOutside(event: MouseEvent): void {
+    let target = event.target as HTMLElement;
+    let navMenuEl = document.getElementById('nav-menu') as HTMLDivElement;
+
+    //console.log(target);
+    if(navMenuEl && !navMenuEl.contains(target)) {
+      this.menuDisplay = 'none';
+    }
+  }
 }
 
-// Variabler
-
-
-// if(openBtn || closeBtn || navMenu) {
-//   openBtn.addEventListener('click', menuToggle);
-//   closeBtn.addEventListener('click', menuToggle);
-// } else {
-//   console.error('Ett eller flera element hittades inte.');
-// }
-
-
-
-// function menuToggle() {
-//   let style = window.getComputedStyle(navMenu);
-
-//   console.log(style);
-//   // if(style.display === 'none') {
-//   //   navMenu.style.display = 'block';
-//   // } else {
-//   //   navMenu.style.display = 'none';
-//   // }
-
-// }
-
-if (window.matchMedia('(max-width: 800px)').matches) {
-  console.log('Skräm är mindre än 800px!');
-} else {
-  console.log('Skärm är större än 800px!');
-}
